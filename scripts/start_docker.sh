0d754d6c36422446b1b7ffe015b44f3d2cd48f19
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-11 23:25:37 +0900

    Update start_docker.sh

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 938652538677.dkr.ecr.ap-northeast-2.amazonaws.com
 # 새로운 이미지를 기반으로 컨테이너 실행
 docker run -d --name my_app_container -p 80:80 938652538677.dkr.ecr.ap-northeast-2.amazonaws
